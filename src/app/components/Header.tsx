cbd50f741561aed4ecd2a4db177e52cd91c293b0
rifky <ee.guy.42@gmail.com>
2023-12-11 14:57:16 +0700

    style: added mobile responsive for header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,20 +10,20 @@ export default async function Header() {
 
   return (
     <nav className="flex flex-row h-20 w-full border border-gray-500">
-      <div className="flex grow-0 flex-col justify-center items-center px-10">
+      <div className="md:flex hidden grow-0 flex-col justify-center items-center px-10">
         <Image src="/trello.png" width={100} height={100} alt="Trello logo" />
       </div>
-      <div className="flex grow-0 flex-row justify-center items-center px-10">
+      <div className="flex grow-0 flex-row justify-center items-center md:px-10">
         <Image
           src="/boards.png"
           width={25}
           height={25}
           alt="Boards icon"
-          className="m-2"
+          className="m-2 w-10"
         />
-        <p>Boards</p>
+        <p className="md:flex hidden">Boards</p>
       </div>
-      <div className="flex grow-0 w-[20%] flex-col justify-center items-center px-10">
+      <div className="flex grow-1 md:grow-0 md:w-[20%] flex-col justify-center items-center px-2 md:px-10">
         <input
           placeholder="Search"
           className="rounded-lg w-[100%] p-1 border border-gray-500 bg-gray-100"
@@ -33,38 +33,38 @@ export default async function Header() {
           width={15}
           height={15}
           alt="search icon"
-          className="absolute self-end m-1"
+          className="absolute self-end m-2"
         />
       </div>
-      <div className="flex grow flex-col justify-center items-center px-10"></div>
-      <div className="flex grow-0 flex-row justify-center items-center">
+      <div className="flex md:grow flex-col justify-center items-center"></div>
+      <div className="flex md:grow-0 grow flex-row md:justify-center justify-end items-center">
         <Image
-          className="mx-2"
+          className="md:mx-2 mx-1"
           src="/plus-circle-outline.png"
           width={25}
           height={25}
           alt="Plus icon"
         />
         <Image
-          className="mx-2"
+          className="md:mx-2 mx-1"
           src="/bell-outline.png"
           width={25}
           height={25}
           alt="bell icon"
         />
         <Image
-          className="mx-2"
+          className="md:mx-2 mx-1"
           src="/alert-circle-outline.png"
           width={25}
           height={25}
           alt="alert icon"
         />
       </div>
-      <div className="flex grow-0 flex-col justify-center items-center px-10">
+      <div className="flex grow-0 flex-col justify-center items-center px-2 md:px-10">
         <Image src="/User.png" width={50} height={50} alt="user profile picture" /> 
         {/* TODO: add user image fetched from backend, use client component */}
       </div>
-      <div className="flex grow-0 flex-col justify-center items-center pr-10">
+      <div className="hidden md:flex grow-0 flex-col justify-center items-center pr-10">
         <LogoutButton />
       </div>
     </nav>
